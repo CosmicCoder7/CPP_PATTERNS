28dda2160fd26f924a6003559777a2f89b3d8748
CosmicCoder7 <r17090994@gmail.com>
2024-10-28 11:57:50 +0530

    PATTERNS

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include "patterns.h"
+
+int main() {
+  SolidRectangle();
+  // Square();
+  // HollowRectangle();
+  // HalfPyramid();
+  // InvertedHalfPyramid();
+  // NumericHalfPyramid();
+  // NumericInvertedPyramid();
+  // Pyramid();
+  // InvertedPyramid();
+  // SolidDiamond();
+  // HollowSolidDiamond();
+  // FlipedSolidDiamond();
+  // FancyPattern();
+  // AlphabetPalindromePyramid();
+  // NumericHollowHalfPyramid();
+  // NumericHollowInvertedHalfPyramid();
+  // NumericPalindromeEquliateralPyramid();
+  // SolidHalfDiamond();
+  // FancyPattern1();
+  // FancyPattern2();
+  // FancyPattern3();
+  // FloydsTrianglePattern();
+  // PascalsTrianglePattern();
+  // ButterflyPattern();
+  return 0;
+}
